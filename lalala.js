32a46e41934791651a83beded3a32118b3411bf9
Andy Chen <andy@Andys-MacBook-Pro.local>
2018-11-10 01:11:32 -0800

    yes

--- lalala.js
+++ lalala.js
@@ -4,6 +4,41 @@ GEankvkekevlnvanvjvalwnvnl elknvklanvenkv
 
 vae
 vaevavae
+awfkbfbhjfwabhfbfw
+fwa
+function(awfkbfbhjfwabhfbfwfw
+	awfkbfbhjfwabhfbfwf
+	awfkbfbhjfwabhfbfwfwafw
+
+	fwaf
+
+
+	) {		
+
+	f
+
+		fwaf
+		functionqf
+		q
+		functionqfw
+		wf
+
+		q
+		wf
+		wfwfwq
+		qfqf
+		f
+
+		fwaff	fwfww	
+		functionqfw
+		fw
+		fwfww
+		wfwfwq
+	}
 
 vaevae
 ve
+
+function function_name(argument) {
+	// body...
+}
